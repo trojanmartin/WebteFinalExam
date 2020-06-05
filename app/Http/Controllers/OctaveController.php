d7749f9ff921adb50be99742221a6219e6c4f01a
Martin <martin.trojan.372@gmail.com>
2020-06-05 14:49:57 +0200

    firt try

--- app/Http/Controllers/OctaveController.php
+++ app/Http/Controllers/OctaveController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
 
 class OctaveController extends Controller
 {
     public function index()
     {
-        $response = shell_exec("octave --no-gui --quiet ../../kyvadlo.txt {r}");
+       // $response = shell_exec("octave --no-gui --quiet ../../kyvadlo.txt {r}");      
+
+        $command = Storage::get('octave/gulicka.txt');        
+
+        $response = ltrim(shell_exec('octave --no-gui --quiet --eval "pkg load control;'. $command .'"'));   
 
         return response($response,200);
     }
